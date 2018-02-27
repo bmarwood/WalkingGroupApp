e3c7ad4021c9fe3cc283d1434e30518984602149
mwdoyle <mwdoyle@sfu.ca>
2018-02-26 21:12:12 -0800

    Add Google maps

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Welcome.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Welcome.java
@@ -1,7 +1,11 @@
 package com.teal.a276.walkinggroup.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.teal.a276.walkinggroup.R;
 
