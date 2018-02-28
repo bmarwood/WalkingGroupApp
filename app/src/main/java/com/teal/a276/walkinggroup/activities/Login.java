1610ce8468ae302dfab4f3a03db6aa32b6819e6d
Brian Marwood <bmarwood.sfu.ca>
2018-02-27 23:08:44 -0800

    create Account button to start create account button activity

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Login.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Login.java
@@ -1,7 +1,11 @@
 package com.teal.a276.walkinggroup.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.teal.a276.walkinggroup.R;
 
@@ -11,6 +15,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupCreateAccountButton();
+    }
+
+    private void setupCreateAccountButton() {
+        Button btn = (Button) findViewById(R.id.createAccntBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = createAccount.makeIntent(Login.this);
+                //StartActivity(intent);
+            }
+        });
+    }
+
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context, Login.class);
     }
 
 
