4f78c27aac81e442c8bbe051519db403cdf3828b
Jamie <jjann@sfu.ca>
2018-02-27 20:49:06 -0800

    Created Monitoring Activity

---
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Monitor.java
@@ -0,0 +1,15 @@
+package com.teal.a276.walkinggroup.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.teal.a276.walkinggroup.R;
+
+public class Monitor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
