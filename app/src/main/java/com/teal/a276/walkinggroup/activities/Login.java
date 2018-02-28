ff817aed63a2e06c73cd4eccf9249f55f77cdb2c
Brian Marwood <bmarwood.sfu.ca>
2018-02-27 22:58:24 -0800

    add Login activity UI Complete

---
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Login.java
@@ -0,0 +1,15 @@
+package com.teal.a276.walkinggroup.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.teal.a276.walkinggroup.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
