acf5f06bfb211dafd89601f39717ae17e7a23b47
Scott Plummer <Scott.Plummer18@gmail.com>
2018-02-27 22:00:02 -0800

    Add basic layout for profile activity

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Profile.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Profile.java
@@ -2,6 +2,8 @@ package com.teal.a276.walkinggroup.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.teal.a276.walkinggroup.R;
 
@@ -11,5 +13,18 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //TODO: get user profile
+        setTextViewText(R.id.username, "PlaceholderName");
+        setTextViewText(R.id.email, "email@placeholder.com");
+    }
+
+    private void setTextViewText(int id, String text) {
+        TextView textView = findViewById(id);
+        textView.setText(text);
+    }
+
+    public void logout(View view) {
+        //TODO: write empty string to shared preference. Pop all views off the stack.
     }
 }
