2b5bd1b31451bf9cba899e74b50a8c808909366b
mwdoyle <mwdoyle@sfu.ca>
2018-03-01 14:10:37 -0800

    Add marker on map to current location
    Use googleApiClient and location services to indicate the users location
    on Google Maps using a marker.

--- app/src/main/java/com/teal/a276/walkinggroup/activities/MapsActivity.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/MapsActivity.java
@@ -2,20 +2,40 @@ package com.teal.a276.walkinggroup.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationListener;
 import com.teal.a276.walkinggroup.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerClickListener,
+        LocationListener {
 
-    private GoogleMap map;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +45,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Instantiates the googleApiClient field if it’s null
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
+        mMap = googleMap;
+
+//        LatLng myPlace = new LatLng(40.73, -73.99);  // New York
+//        mMap.addMarker(new MarkerOptions().position(myPlace).title("My Favorite City"));
+//        // Supports zoom levels 0-20
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12));
+
+        // Add zoom controls /  make marker clickable
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMarkerClickListener(this);
     }
 
     public static Intent makeIntent(Context context) {
@@ -37,4 +75,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return intent;
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Initiates a background connection of the client to Google Play services.
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Closes the connection to Google Play services if the client is not null and is connected
+        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        // Enables the my-location layer which draws a light blue dot on the user’s location
+        mMap.setMyLocationEnabled(true);
+
+        // Set map type
+        // TODO: Causes problems with custom icon
+//        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+
+        // Determines the availability of location data on the device
+        LocationAvailability locationAvailability =
+                LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient);
+        if (null != locationAvailability && locationAvailability.isLocationAvailable()) {
+            // Gives you the most recent location currently available
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            // Move the camera to the user’s current location
+            if (mLastLocation != null) {
+                LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation
+                        .getLongitude());
+                // Add pin at user's location
+                placeMarkerOnMap(currentLocation);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12));
+            }
+        }
+    }
+
+    protected void placeMarkerOnMap(LatLng location) {
+
+        // Create a MarkerOptions object and sets the user’s current location as the position for the marker
+        MarkerOptions markerOptions = new MarkerOptions().position(location);
+
+        // Add the marker to the map
+        mMap.addMarker(markerOptions);
+
+
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        setUpMap();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
 }
