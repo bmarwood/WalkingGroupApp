29641b4645b08a92dc1efb26bfc22a15c43a3a95
bmarwood <bmarwood@sfu.ca>
2018-03-07 14:26:49 -0800

    checks for input that has more than nothing in input

--- app/src/main/java/com/teal/a276/walkinggroup/activities/CreateAccount.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/CreateAccount.java
@@ -4,8 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.teal.a276.walkinggroup.R;
+import com.teal.a276.walkinggroup.models.UserModel;
+
+import java.util.ArrayList;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -13,6 +23,44 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        setupCreateButton();
+    }
+
+    private void setupCreateButton() {
+        Button btn = (Button) findViewById(R.id.createAccntBtn);
+        final ProgressBar spinner = (ProgressBar) findViewById(R.id.progressBarCreate);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = ((EditText) findViewById(R.id.firstName)).getText().toString();
+                String lastName = ((EditText) findViewById(R.id.lastName)).getText().toString();
+                String email = ((EditText) findViewById(R.id.email)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        spinner.setVisibility(View.VISIBLE);
+                    }
+                });
+                ArrayList<String> errors =  UserModel.checkCreateInputs(firstName, lastName, email, password);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        spinner.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+                if(!errors.isEmpty()){
+                    TextView errorsForUser = (TextView) findViewById(R.id.badEmailOrPassword);
+
+                }
+
+
+
+
+            }
+        });
     }
 
     public static Intent makeIntent(Context context) {
--- app/src/main/java/com/teal/a276/walkinggroup/models/UserModel.java
+++ app/src/main/java/com/teal/a276/walkinggroup/models/UserModel.java
@@ -7,6 +7,11 @@ import android.widget.Toast;
 
 import com.teal.a276.walkinggroup.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Brian on 3/5/2018.
  */
@@ -14,12 +19,41 @@ import com.teal.a276.walkinggroup.R;
 public class UserModel {
     private static String userEmail = "1";
     private static String userPassword = "2";
+
     public static boolean checkInputs(String email, String password) {
-        if(email.equals(userEmail) && password.equals(userPassword)) {
+        if (email.equals(userEmail) && password.equals(userPassword)) {
             return true;
+        } else return false;
+    }
+
+    public static ArrayList<String> checkCreateInputs(String firstName, String lastName, String email, String password) {
+        ArrayList<String> returnErrorMessages = new ArrayList<String>();
+
+        if (firstName.length() == 0) {
+            returnErrorMessages.add("First Name must be inputted");
         }
-        else return false;
+        if (lastName.length() == 0) {
+            returnErrorMessages.add("Last Name must be inputted");
+
+        }
+        if (email.length() == 0) {
+            returnErrorMessages.add("Email must be inputted");
+
+        }
+        if(password.length() == 0) {
+            returnErrorMessages.add("password must be inputted");
+
+        }
+        if(email.length() > 0) {
+            Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+            Matcher m = p.matcher(email);
+            if (! m.matches()) {
+                returnErrorMessages.add("Email must be formatted correctly");
+            }
+        }
+        return returnErrorMessages;
     }
     //TODO: interact with server to check if email and string are correct
+
 
 }
