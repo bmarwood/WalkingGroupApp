97d4d64cfe070a18b1f446c4aa6a76af6abc9a7e
Brian Marwood <bmarwood.sfu.ca>
2018-03-14 01:00:30 -0700

    login loading screen implemented, Fixs LOTS of bugs

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Login.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Login.java
@@ -21,22 +21,28 @@ import com.teal.a276.walkinggroup.model.serverproxy.ServerManager;
 import com.teal.a276.walkinggroup.model.serverproxy.ServerProxy;
 import com.teal.a276.walkinggroup.model.serverrequest.requestimplementation.CompleteUserRequest;
 
+import org.w3c.dom.Text;
+
 import retrofit2.Call;
 
 public class Login extends BaseActivity {
     User user = new User();
     TextView errorsForUser;
+    TextView signInError;
+    Boolean layoutSwitchForLogin = false;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkForLogin();
         setContentView(R.layout.activity_login);
+        checkForLogin();
 
         errorsForUser = findViewById(R.id.errorInput);
         errorsForUser.setTextColor(Color.RED);
+        signInError = findViewById(R.id.issueSignInTxt);
+        signInError.setTextColor(Color.RED);
         
         setUpPermissions();
         setUpLoginButton();
@@ -46,17 +52,54 @@ public class Login extends BaseActivity {
     private void checkForLogin() {
         SharedPreferences prefs = getSharedPreferences("loggedIn",MODE_PRIVATE);
         Boolean extractedBool = prefs.getBoolean("Logged in", false);
+        layoutSwitchForLogin = extractedBool;
         String userName = prefs.getString("email", null);
         String password = prefs.getString("password", null);
         Toast.makeText(this, extractedBool.toString(), Toast.LENGTH_SHORT).show();
 
         if(extractedBool){
+            LayoutLoggingIn(true);
             user.setEmail(userName);
             user.setPassword(password);
             ServerProxy proxy = ServerManager.getServerRequest();
             Call<Void> caller = proxy.login(user);
             ServerManager.serverRequest(caller, Login.this::successLogin, Login.this::errorLogin);
         }
+    }
+
+    private void LayoutLoggingIn(boolean on) {
+        TextView txtLogin = findViewById(R.id.loginText);
+        TextView txtEmnail = findViewById(R.id.emailTxt);
+        TextView txtPassword = findViewById(R.id.passwordTxt);
+        TextView txtCreate = findViewById(R.id.createAccountTxt);
+        EditText editEmail = findViewById(R.id.emailEditText);
+        EditText editPassword = findViewById(R.id.passwordEditText);
+        Button createAccountbtn = findViewById(R.id.createAccntBtn);
+        Button signInbtn = findViewById(R.id.signInBtn);
+
+        ProgressBar loginSpin = findViewById(R.id.loggingInSpinner);
+        TextView txtLoggin = findViewById(R.id.loggingInText);
+        int main = 0;
+        int opposite = 0;
+        if(on) {
+            main = View.INVISIBLE;
+            opposite = View.VISIBLE;
+        }else{
+            main = View.VISIBLE;
+            opposite = View.INVISIBLE;
+        }
+            txtLogin.setVisibility(main);
+            txtEmnail.setVisibility(main);
+            txtPassword.setVisibility(main);
+            txtCreate.setVisibility(main);
+            editEmail.setVisibility(main);
+            editPassword.setVisibility(main);
+            createAccountbtn.setVisibility(main);
+            signInbtn.setVisibility(main);
+
+            loginSpin.setVisibility(opposite);
+            txtLoggin.setVisibility(opposite);
+
     }
 
     private void setUpPermissions() {
@@ -68,6 +111,10 @@ public class Login extends BaseActivity {
     }
 
     private void setUpLoginButton() {
+        if(layoutSwitchForLogin){
+            TextView signinError = findViewById(R.id.issueSignInTxt);
+            signinError.setVisibility(View.INVISIBLE);
+        }
         Button btn = findViewById(R.id.signInBtn);
         btn.setOnClickListener(v -> {
             EditText emailInput = findViewById(R.id.emailEditText);
@@ -141,9 +188,17 @@ public class Login extends BaseActivity {
     }
 
     private void errorLogin(String error) {
-        TextView errorsForUser = findViewById(R.id.errorInput);
-        errorsForUser.setVisibility(View.VISIBLE);
-        toggleSpinner(View.INVISIBLE);
+        if(layoutSwitchForLogin){
+            LayoutLoggingIn(false);
+            TextView signinError = findViewById(R.id.issueSignInTxt);
+            signinError.setVisibility(View.VISIBLE);
+            layoutSwitchForLogin = false;
+        }
+        else {
+            TextView errorsForUser = findViewById(R.id.errorInput);
+            errorsForUser.setVisibility(View.VISIBLE);
+            toggleSpinner(View.INVISIBLE);
+        }
     }
 
     private void toggleSpinner(int visibility) {
