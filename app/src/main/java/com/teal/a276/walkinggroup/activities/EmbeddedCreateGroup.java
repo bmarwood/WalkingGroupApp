623fb951f121189ce0ddfe89312628038b8d3ae9
Jamie <jjann@sfu.ca>
2018-03-17 16:44:48 -0700

    EmbeddedCreateGroup now centers and pins at user's current location.

--- app/src/main/java/com/teal/a276/walkinggroup/activities/EmbeddedCreateGroup.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/EmbeddedCreateGroup.java
@@ -1,27 +1,40 @@
 package com.teal.a276.walkinggroup.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.teal.a276.walkinggroup.R;
 
 public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallback,
-GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnMarkerClickListener,
         LocationListener {
@@ -35,6 +48,8 @@ GoogleApiClient.ConnectionCallbacks,
     private Location lastLocation;
     private LatLng currentLocation;
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +58,7 @@ GoogleApiClient.ConnectionCallbacks,
 
 
         Bundle mapViewBundle = null;
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
         }
 
@@ -59,33 +74,128 @@ GoogleApiClient.ConnectionCallbacks,
                     .build();
         }
 
-
+        createLocationRequest();
     }
 
 
+
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();;
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
+                        builder.build());
+
+        result.setResultCallback(result1 -> {
+            final Status status = result1.getStatus();
+            switch (status.getStatusCode()) {
+                case LocationSettingsStatusCodes.SUCCESS:
+                    startLocationUpdates();
+                    break;
+                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                    try {
+                        status.startResolutionForResult(this, 2);
+                    } catch (IntentSender.SendIntentException e) {
+                    }
+                    break;
+                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                    break;
+            }
+        });
+    }
+
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,
+                this);
+    }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.setMinZoomPreference(12);
-        LatLng ny = new LatLng(40.7143528, -74.0059731);
-        map.moveCamera(CameraUpdateFactory.newLatLng(ny));
+
+        //map.setMinZoomPreference(9);
+        //LatLng ny = new LatLng(40.7143528, -74.0059731);
+        //map.moveCamera(CameraUpdateFactory.newLatLng(ny));
+
+        //map.addMarker(new MarkerOptions().
+        //        position(ny).
+        //        draggable(true));
+        //Log.d("initial location", "Lat" + currentLocation.latitude + "Lng" + currentLocation.longitude);
+
+
+
     }
     @Override
     protected void onStart(){
         super.onStart();
+        googleApiClient.connect();
         mapView.onStart();
     }
 
-    public static Intent makeIntent(Context context) {
+    public static Intent makeIntent(Context context){
 
         return new Intent(context, EmbeddedCreateGroup.class);
     }
 
+    private void setUpMap(){
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        map.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        LocationAvailability locationAvailability =
+                LocationServices.FusedLocationApi.getLocationAvailability(googleApiClient);
+        if (null != locationAvailability && locationAvailability.isLocationAvailable()) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+                currentLocation = new LatLng(lastLocation.getLatitude(), lastLocation
+                        .getLongitude());
+                currentLocation = locationToLatLng();
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12));
+            }
+        }
+        map.addMarker(new MarkerOptions().
+                position(currentLocation).
+                draggable(true));
+        Log.d("initial location", "Lat" + currentLocation.latitude + "Lng" + currentLocation.longitude);
+
+
+    }
+
+
+    @NonNull
+    private LatLng locationToLatLng(){
+        return new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+    }
+
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        setUpMap();
+        startLocationUpdates();
     }
 
     @Override
@@ -100,11 +210,12 @@ GoogleApiClient.ConnectionCallbacks,
 
     @Override
     public void onLocationChanged(Location location) {
-
+        lastLocation = location;
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
         return false;
     }
+
 }
