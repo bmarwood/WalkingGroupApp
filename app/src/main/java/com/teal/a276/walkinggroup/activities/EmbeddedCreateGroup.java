0f6475466122df91ba149c81fa697d6e4e15a151
Jamie <jjann@sfu.ca>
2018-03-18 12:14:33 -0700

    Added textfields, extraction of lat lng from mapView, buttons

--- app/src/main/java/com/teal/a276/walkinggroup/activities/EmbeddedCreateGroup.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/EmbeddedCreateGroup.java
@@ -11,6 +11,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -31,6 +35,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.teal.a276.walkinggroup.R;
+import com.teal.a276.walkinggroup.model.dataobjects.User;
+
+import java.util.Observer;
 
 public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -46,12 +53,16 @@ public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallb
     private LocationRequest locationRequest;
     private Location lastLocation;
     private LatLng currentLocation;
+    double lat = 0;
+    double lng = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_embedded_create_group);
 
+
+        setupCreateButton();
 
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
@@ -73,8 +84,59 @@ public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallb
         createLocationRequest();
     }
 
+    private void setupCreateButton() {
+        Button btn = findViewById(R.id.embeddedCreateButton);
+        btn.setOnClickListener(v ->{
+            EditText nameVal = findViewById(R.id.embeddedNameEdit);
+            String nameValStr = nameVal.getText().toString();
+
+            EditText leadersEmailVal = findViewById(R.id.embeddedEmailEdit);
+            String leadersEmailStr = leadersEmailVal.getText().toString();
+
+            if(nameValStr.isEmpty()){
+                nameVal.setError(getString(R.string.empty_group_name));
+                return;
+            }
+            if(!User.validateEmail(leadersEmailStr)){
+                leadersEmailVal.setError(getString(R.string.invalid_email));
+                return;
+            }
+            if((lat==0) && (lng==0)){
+                Toast.makeText(
+                        EmbeddedCreateGroup.this,
+                        "Location not set",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            //Server
+            //after implementing server code, comment out code in setGroupResult method below
 
 
+            //finish();
+        });
+    }
+
+    //Locations Methods
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        Log.d("Lat Long", "Lat: " + lat + "Long: " + lng);
+
+        map.setOnMapClickListener(latLng -> {
+            //clear any previous marker
+            map.clear();
+            map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            map.addMarker(new MarkerOptions().position(latLng));
+
+            lat = latLng.latitude;
+            lng = latLng.longitude;
+            Log.d("Lat Long", "Lat: " + lat + "Long: " + lng);
+        });
+
+    }
     private void createLocationRequest() {
         locationRequest = new LocationRequest();;
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
@@ -104,7 +166,6 @@ public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallb
         });
     }
 
-
     protected void startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -113,35 +174,15 @@ public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallb
                     LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
-
         LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,
                 this);
     }
 
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-
-        map.setOnMapClickListener(latLng -> {
-            //clear any previous marker
-            map.clear();
-            map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            map.addMarker(new MarkerOptions().position(latLng));
-        });
-
-    }
     @Override
     protected void onStart(){
         super.onStart();
         googleApiClient.connect();
         mapView.onStart();
-    }
-
-    public static Intent makeIntent(Context context){
-
-        return new Intent(context, EmbeddedCreateGroup.class);
     }
 
     private void setUpMap(){
@@ -205,6 +246,20 @@ public class EmbeddedCreateGroup extends BaseActivity implements OnMapReadyCallb
     @Override
     public boolean onMarkerClick(Marker marker) {
         return false;
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        return true;
+    }
+    public static void setGroupResultCallback(Observer obs){
+        //newGroupObserver = obs;
+    }
+    //delete later
+    public static Intent makeIntent(Context context){
+        return new Intent(context, EmbeddedCreateGroup.class);
     }
 
 }
