ebe52fed77280267da91f1400ce4ca471540db78
bmarwood <bmarwood@sfu.ca>
2018-03-28 13:28:43 -0700

    chages to tableRow Layout

--- app/src/main/java/com/teal/a276/walkinggroup/activities/map/DashBoard.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/map/DashBoard.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,6 +23,7 @@ import com.teal.a276.walkinggroup.model.dataobjects.User;
 import com.teal.a276.walkinggroup.model.dataobjects.UserLocation;
 import com.teal.a276.walkinggroup.model.serverproxy.ServerManager;
 import com.teal.a276.walkinggroup.model.serverproxy.ServerProxy;
+import com.teal.a276.walkinggroup.model.serverproxy.ServerResult;
 
 import java.util.List;
 import java.util.Observable;
@@ -92,11 +95,17 @@ public class DashBoard extends AbstractMapActivity implements Observer {
 
     private void monitorsResult(List<User> users) {
         for(User user: users) {
-            addMonitorName(user);
 
             ServerProxy proxy = ServerManager.getServerRequest();
             Call<UserLocation> call = proxy.getLastGpsLocation(user.getId());
-            ServerManager.serverRequest(call, result -> placeMonitorsOnMap(result, user.getName()), this::error);
+            ServerManager.serverRequest(call, new ServerResult<UserLocation>() {
+                @Override
+                public void result(UserLocation result) {
+                    DashBoard.this.addMonitorName(user.getName(), result.getTimestamp(), user.getId());
+                    DashBoard.this.placeMonitorsOnMap(result, user.getName());
+                }
+            }, this::error);
+
 
             List<Group> groups = user.getMemberOfGroups();
             for(Group group : groups){
@@ -144,12 +153,25 @@ public class DashBoard extends AbstractMapActivity implements Observer {
 
     }
 
-    private void addMonitorName(User user) {
-        LinearLayout scrollDash = (LinearLayout) findViewById(R.id.dashboardBottomMonitorees);
+    private void addMonitorName(String name, String timestamp, Long id) {
+        TableLayout scrollDash = (TableLayout) findViewById(R.id.monitorsTable);
+
+        TableRow row= new TableRow(this);
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(layoutParams);
+
+
         TextView leader = new TextView(this);
-        leader.setText(user.getName());
-        leader.setId(user.getId().intValue());
-        scrollDash.addView(leader);
+        leader.setText("Sally");
+        leader.setId(id.intValue());
+        row.addView(leader);
+
+
+        TextView gps = new TextView(this);
+        leader.setText(timestamp);
+        leader.setId(user.getId().intValue()+1);
+        row.addView(gps);
+        scrollDash.addView(row);
     }
 
 
