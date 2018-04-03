5b13f3dd5aa68d9468f99bf334a7fd36005ad0cf
Scott Plummer <Scott.Plummer18@gmail.com>
2018-04-02 18:32:35 -0700

    Add basic activity for permissions

---
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/PermissionRequest.java
@@ -0,0 +1,14 @@
+package com.teal.a276.walkinggroup.activities;
+
+import android.os.Bundle;
+
+import com.teal.a276.walkinggroup.R;
+
+public class PermissionRequest extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_request);
+    }
+}
