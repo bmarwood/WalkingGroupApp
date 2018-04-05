8b437a85e945bd7129090070b37fc3cef2b16908
Jamie <jjann@sfu.ca>
2018-04-05 14:25:54 -0700

    Leaderboard now sorts Max to min, strips list to 100 if more than 100 users.

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Leaderboard.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Leaderboard.java
@@ -15,6 +15,8 @@ import com.teal.a276.walkinggroup.R;
 import com.teal.a276.walkinggroup.model.dataobjects.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -35,16 +37,41 @@ public class Leaderboard extends BaseActivity {
 
     private void initializeLeaderboardListView() {
 
-        User bob = new User();
-        bob.setName("Bob");
-        bob.setTotalPointsEarned(1000);
+        User bob1 = new User();
+        bob1.setName("Bob1");
+        bob1.setTotalPointsEarned(1);
 
-        testingUsers.add(bob);
+        User bob2 = new User();
+        bob2.setName("Bob2");
+        bob2.setTotalPointsEarned(2);
+
+        User bob3 = new User();
+        bob3.setName("Bob3");
+        bob3.setTotalPointsEarned(3);
+
+        User bob4 = new User();
+        bob4.setName("Bob4");
+        bob4.setTotalPointsEarned(4);
+
+        testingUsers.add(bob1);
+        testingUsers.add(bob4);
+        testingUsers.add(bob2);
+        testingUsers.add(bob3);
+
+        System.out.println(Arrays.deepToString(testingUsers.toArray()));
+
+        //Sort users from max to min
+        Collections.sort(testingUsers, ((o1, o2) -> o2.getTotalPointsEarned()
+                .compareTo(o1.getTotalPointsEarned())));
+
+        //shrink size of listArray to 100 (top 100 users)
+        if(testingUsers.size()>100){
+            testingUsers.subList(100, testingUsers.size());
+        }
 
         leaderboardAdapter = new ListItemAdapter(this, testingUsers, true);
         ListView leaderboardList = findViewById(R.id.leaderboardLv);
         leaderboardList.setAdapter(leaderboardAdapter);
-
     }
 
 
@@ -66,12 +93,14 @@ public class Leaderboard extends BaseActivity {
             View itemView = convertView;
             if (itemView == null) {
                 LayoutInflater inflater = LayoutInflater.from(context);
-                itemView = inflater.inflate(R.layout.list_item, parent, false);
+                itemView = inflater.inflate(R.layout.leaderboard_list, parent, false);
             }
             User selectedUser = listItems.get(position);
 
             TextView nameTextView = itemView.findViewById(R.id.userName);
-            nameTextView.setText(selectedUser.getName());
+            String displayText = selectedUser.getName() + "    -    " +
+                    selectedUser.getTotalPointsEarned() + " Points";
+            nameTextView.setText(displayText);
 
             return itemView;
         }
