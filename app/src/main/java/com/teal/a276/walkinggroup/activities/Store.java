63116c86e04755d58498adef4a7d2a427d2b4fad
Brian Marwood <bmarwood.sfu.ca>
2018-04-06 00:32:35 -0700

    swaps theme mid app

--- app/src/main/java/com/teal/a276/walkinggroup/activities/Store.java
+++ app/src/main/java/com/teal/a276/walkinggroup/activities/Store.java
@@ -2,10 +2,13 @@ package com.teal.a276.walkinggroup.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,11 +18,23 @@ import com.teal.a276.walkinggroup.model.dataobjects.User;
 
 public class Store extends BaseActivity implements View.OnClickListener {
 
+    private static final String PREFS_NAME = "prefs";
+    private static final String GREEN_BOX_THEME = "green_box_theme";
+
+
     private User user;
     private int remainingPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(GREEN_BOX_THEME, false);
+
+        if(useDarkTheme) {
+            setTheme(R.style.AppTheme_Dark);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
@@ -29,6 +44,25 @@ public class Store extends BaseActivity implements View.OnClickListener {
         setupItemClickListeners();
         setupPurchaseButtonClickListener();
         setupDefaultBtn();
+        Switch toggle = (Switch) findViewById(R.id.switch1);
+        toggle.setChecked(useDarkTheme);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                toggleTheme(isChecked);
+            }
+        });
+    }
+
+    private void toggleTheme(boolean darkTheme) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(GREEN_BOX_THEME, darkTheme);
+        editor.apply();
+
+        Intent intent = getIntent();
+        finish();
+
+        startActivity(intent);
     }
 
     private void setupDefaultBtn() {
